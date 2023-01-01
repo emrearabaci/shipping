8c03d6ccaf8e9ef55f29d18a84074e75bed0811f
Emre Arabaci <a.emrearabaci@gmail.com>
2023-01-02 01:28:34 +0300

    Update [shipmentCode].js
    Added "updateShipmentRequest" function to update for selected shipment.

--- pages/shipments/[shipmentCode].js
+++ pages/shipments/[shipmentCode].js
@@ -1,17 +1,89 @@
+/* React API */
+import { useState } from 'react';
+
+/* Next API */
+import { useRouter } from 'next/router';
+
 /* Static Props */
 import { loadShipments } from '../../lib/loadShipments';
+
+/* Hooks */
+import { useForm } from '../../hooks/useForm';
 
 /* Instances */
 import axios from '../../configs/axiosInstance';
 
+/* Components */
+import SelectField from '../../components/SelectField';
+import Button from '../../components/Button';
+
 export default function ShipmentDetail({ selectedShipment }) {
+  const router = useRouter();
+
+  const [errors, setErrors] = useState([]);
+  const [shipmentUpdateForm, setShipmentUpdateForm] = useForm({
+    shipmentCode: selectedShipment[0].shipmentCode,
+    shipmentStatus: ''
+  });
+
+  const shipmentStatusOptions = [
+    {
+      id: 0,
+      name: 'Shipment Status Options',
+      units: [
+        { id: 0, value: 'q', label: 'In queue' },
+        { id: 1, value: 'p', label: 'In Progress' },
+        { id: 2, value: 'c', label: 'Completed' }
+      ]
+    }
+  ];
+
+  const updateShipmentRequest = async (event) => {
+    event.preventDefault(event);
+    const data = {
+      shipmentUpdateForm
+    };
+
+    await axios
+      .post('/shipment/updateShipment', data, {
+        withCredentials: false
+      })
+      .then((res) => {
+        console.table(res.data);
+        if (res.status === 200) {
+          router.replace(router.asPath);
+        }
+      })
+      .catch((err) => {
+        const errors = err.response.data.error;
+        console.log(errors);
+        setErrors(errors);
+      });
+  };
+
   return (
     <>
       {selectedShipment.map((shipment, index) => (
         <div key={index}>
           <p>{shipment.shipmentCode}</p>
+          <p>{shipment.shipmentStatus}</p>
         </div>
       ))}
+
+      <form>
+        <SelectField
+          name={'shipmentStatus'}
+          placeholder={'Please Select a Shipment Status'}
+          value={shipmentUpdateForm.shipmentStatus}
+          options={shipmentStatusOptions}
+          onChange={setShipmentUpdateForm}
+        />
+        <Button
+          type={`submit`}
+          onClick={updateShipmentRequest}
+          content={`Update Status`}
+        />
+      </form>
     </>
   );
 }
